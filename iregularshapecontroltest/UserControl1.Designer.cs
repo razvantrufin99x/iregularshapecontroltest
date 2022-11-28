65396ecf7227cbeed951432e8fbb100d68da1b58
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 20:27:19 +0200

    Add project files.

---
+++ iregularshapecontroltest/UserControl1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace iregularshapecontroltest
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(148, 148);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UserControl1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UserControl1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UserControl1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ iregularshapecontroltest/UserControl1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace iregularshapecontroltest
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public bool ismd = false;
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            Point[] pointsA = new Point[]
+{
+new Point(0, 0),
+new Point(40, 60),
+new Point(this.Width - 100, 10)
+};
+            path.AddCurve(pointsA);
+            Point[] pointsB = new Point[]
+{
+new Point(this.Width - 40, this.Height - 60),
+new Point(this.Width, this.Height),
+new Point(10, this.Height)
+};
+            path.AddCurve(pointsB);
+            path.CloseAllFigures();
+            this.Region = new Region(path);
+        }
+
+        private void UserControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ismd = true;
+        }
+
+        private void UserControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ismd = false;
+        }
+
+        private void UserControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ismd == true)
+            {
+                this.Left += e.X;
+                this.Top += e.Y;
+            }
+        }
+        }
+    }
+
